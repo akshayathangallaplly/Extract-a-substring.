4c4e3b8071b2f81777466a53d18d46e5c773cdec
akshayathangallapally <ta7440360@gmail.com>
2025-01-28 23:34:56 +0530

    Add files via upload

---
+++ Substr.java
@@ -0,0 +1,8 @@
+public class Substr {
+    public static void main(String[] args) {
+        String str="Hello World";
+        String substring=str.substring(5);
+        System.out.println("Substring:"+substring);
+    }
+    
+}
